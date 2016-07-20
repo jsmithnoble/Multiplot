2945ed1546cfd126ab7bffc68e50878ae9b2854d
Jake Noble <jsmithnoble@gmail.com>
2016-07-20 08:32:16 -0600

    pulled new events into csvs and build function to graph individual users.

--- EDA_multitude.py
+++ EDA_multitude.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import numpy as np
 import requests
 import json
 import folium
+from time import time
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
 import cPickle as pickle
 import os
 
@@ -14,22 +18,13 @@ def get_data():
     event = pd.read_csv("event.csv", names = event_cols)
     event['in_ny'] = (event['lat']>= 40.47739) & (event['lat']<=40.917577) & (event['long'] >= -74.25909) & (event['long'] <= -73.700009)
     event.set_index('event_id', inplace =True)
-    event = event.drop(['two_again', 'device_type_again', 'device_id_again', 'zero_again'], axis=1)
     event['timestamp'] = pd.to_datetime(event['10-digit-int'] - 4 * 60 * 60, unit = 's')#, format = "%Y-%m-%d")
+    event = event.drop(['two_again', 'device_type_again', 'device_id_again', 'zero_again', '10-digit-int','url_session_num','int_2_to_1101','file_path?','NaNs','zero','weird_hash_thing','two','13-digit_int'], axis=1)
     readings_cols = ['event_id','value','measurment']
     readings = pd.read_csv("event_readings.csv", names = readings_cols)
     readings.set_index('event_id', inplace = True)
     readings = readings.pivot(columns = 'measurment', values = 'value')
-    return event, readings
-
-def get_single_device(device_id, data):
-    data = data[data['device_id'] == device_id].sort('timestamp')
-    time_delta = [0]
-    for idx, time in enumerate(data.timestamp):
-        if idx == 0:
-            pass
-        else:
-            time_delta.append(time - data.timestamp.iloc[idx - 1])
+    return event.join(readings).sort_values('timestamp')
 
 def is_in_ny(data):
     '''
@@ -45,13 +40,33 @@ def is_in_ny(data):
     return data
 
 
+def get_location(coords):
+    '''
+    INPUT: list of tuples containing (latitude, longitude)
+    OUTPUT: list of strings containing response from openstreetmap
 
-"""46140 events
-141 device_id shows 141 different devices --I think I can only keep AirBeams (18,712) and Terriers (1,879)
-device_type says all devices are either AirBeams or Terriers
-"""
+    This function uses a reverse geocode lookup from https://nominatim.openstreetmap.org
+    '''
+    start = time()
+    loc = []
+    ct = 0
+    for lat, lon in coords:
+        if ct % 100 == 0:
+            print "on event {}".format(ct)
+        ct += 1
+        try:
+            r = requests.get("https://nominatim.openstreetmap.org/reverse.php?format=html&lat={}&lon={}&zoom=".format(lat,lon))
+            soup = BeautifulSoup(r.content, 'html.parser')
+            loc.append(soup.find_all(class_="name")[0].text)
+        except:
+            loc.append("not available")
+    print "completed in {} seconds".format(time() - start)
+    return loc
 
 def get_city_state_country(coords):
+    '''
+    mostly useless because google api is stingy
+    '''
     gmaps_key = os.environ.get("gmaps_key")
     json_decoder = json.JSONDecoder()
     cities = []
@@ -81,7 +96,67 @@ def get_city_state_country(coords):
     pickle.dump(countries, open('countries.pkl', 'wb'))
     return cities, states, countries
 
+def get_single_device(device_id):
+    '''
+    return a subset of data where device_id == device_id
+    '''
+    data = event[event['device_id'] == device_id].sort_values('timestamp')
+    time_delta = [pd.tslib.Timedelta(1e3, unit = 'm')]
+    for idx, time in enumerate(data.timestamp):
+        if idx == 0:
+            pass
+        else:
+            time_delta.append(time - data.timestamp.iloc[idx - 1])
+    data['time_since_last_pt'] = time_delta
+    ride_break = pd.tslib.Timedelta(15, unit = 'm')
+    ride_num = 0
+    ride = []
+    for delta in data['time_since_last_pt']:
+        if delta < ride_break:
+            ride.append(ride_num)
+        else:
+            ride_num += 1
+            ride.append(ride_num)
+    data['ride'] = ride
+    return data
+
 def plot_single_device(device_id):
+    '''
+    INPUT device_id: string
+    OUTPUT: None
+    save plot of a single device_id
+    '''
+    data = get_single_device(device_id)
+    rides = data.ride.unique()
+    lat = data.lat.mean()
+    lon = data.long.mean()
+    m = folium.Map((lat,lon), zoom_start = 12)
+    for ride in rides:
+        temp = data[data.ride == ride]
+        coords = zip(temp.lat,temp.long)
+        time = temp.timestamp
+        plot_single_ride(m, coords, time)
+    m.save("plots/devices/{}".format(device_id))
+
+def plot_single_ride(m, coords, time = None, interval = 5):
+    '''
+    INPUT:  m = folium Map object to be plotted on
+            coords = list of tuples containing (latitude, longitude)
+            time  = pandas series object containing timestamps which correspond to the coords
+            interval = interval in minutes to place markers must be integer >= 1
+    OUTPUT: folium Map object with polyline of the ride and markers if time is specified
+    '''
+    folium.PolyLine(coords).add_to(m)
+
+    if not isinstance(time,type(None)):
+        for idx, t in enumerate(time.astype(str)):
+            if idx % interval == 0:
+                folium.Marker(coords[idx], popup = t).add_to(m)
+    folium.Marker(coords[0], popup = "Start").add_to(m)
+    folium.Marker(coords[-1], popup = "End").add_to(m)
+    return m
+
+# def
 
 # terminal command to connect to db and download event table and convert to csv
 # psql -h 104.196.143.165 -U multitude -d d2c_api_prod -p 5432 -o event.csv -c 'select * from event;' -P format=unaligned -P tuples_only -P fieldsep=\,
@@ -90,19 +165,27 @@ def plot_single_device(device_id):
 # psql -h <host_ip> -U <username> -d <database> -p <port> -o <output_file_path> -c '<query>' -P format=unaligned -P tuples_only -P fieldsep=\,
 
 if __name__ == '__main__':
-    event, readings = get_data()
-    coords = zip(event.lat,event.long)
-    # city, state, country = get_city_state_country(coords)
-    d1 = get_single_device('SGH-M919:1070',event)
+    event = get_data()
+    # coords = zip(event.lat,event.long)
+    # locations = get_location(coords)
+    # event['location'] = locations
+    pickle.dump(event, open('event.pkl', 'wb'))
+    d1 = get_single_device('SAMSUNG-SM-G920A:1083')
+    ny = event[event['in_ny'] == True]
+    # lat = d1.lat.mean()
+    # lon = d1.long.mean()
+    # days = d1.timestamp.dt.date.unique()
+    # for n, day in enumerate(days):
+    #     m = folium.Map((lat,lon), zoom_start = 13)
+    #     temp = d1[d1.timestamp.dt.date == day]
+    #     coords = zip(temp.lat,temp.long)
+    #     folium.PolyLine(coords).add_to(m)
+    #     folium.Marker(coords[0], popup = 'Start').add_to(m)
+    #     folium.Marker(coords[-1], popup = "End").add_to(m)
+    # m.save('plots/devices/{}.html'.format(device_id))
+    """Same user for 'Nexus-6P:1008' and 'AirBeam:001896105F77'"""
 
-    lat = d1.lat.mean()
-    lon = d1.long.mean()
-    days = d1.timestamp.dt.date.unique()
-    for n, day in enumerate(days):
-        m = folium.Map((lat,lon), zoom_start = 13)
-        temp = d1[d1.timestamp.dt.date == day]
-        coords = zip(temp.lat,temp.long)
-        folium.PolyLine(coords).add_to(m)
-        folium.Marker(coords[0], popup = 'Start').add_to(m)
-        folium.Marker(coords[-1], popup = "End").add_to(m)
-    m.save('plots/devices/{}.html'.format(device_id))
+    # plot_single_device('Nexus-6P:1008')
+    # plot_single_device('AirBeam:001896105F77')
+    # d2 = get_single_device('Nexus-6P:1008')
+    # d3 = get_single_device('AirBeam:001896105F77')
