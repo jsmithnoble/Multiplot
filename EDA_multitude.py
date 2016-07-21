994f91666ab517c3a471056367400014a5c0254a
Jake Noble <jsmithnoble@gmail.com>
2016-07-20 18:07:29 -0600

    Sweet clustering algorithm for finding cities, clustering in cities needs tweeks

--- EDA_multitude.py
+++ EDA_multitude.py
@@ -8,15 +8,17 @@ import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 import cPickle as pickle
 import os
+import geopy
+from sklearn.cluster import KMeans, DBSCAN
+
 
 def get_data():
     '''
-    return event and event_readings data frames
+    return event merged with event_readings
     '''
     event_cols = ['event_id', 'weird_hash_thing', 'two', 'device_id', 'zero', 'NaNs', 'file_path?',
-    '13-digit_int', 'lat', 'long', 'device_type', 'two_again', 'device_type_again','device_id_again', 'zero_again', 'url_session_num', '10-digit-int','int_2_to_1101']
+    '13-digit_int', 'lat', 'lon', 'device_type', 'two_again', 'device_type_again','device_id_again', 'zero_again', 'url_session_num', '10-digit-int','int_2_to_1101']
     event = pd.read_csv("event.csv", names = event_cols)
-    event['in_ny'] = (event['lat']>= 40.47739) & (event['lat']<=40.917577) & (event['long'] >= -74.25909) & (event['long'] <= -73.700009)
     event.set_index('event_id', inplace =True)
     event['timestamp'] = pd.to_datetime(event['10-digit-int'] - 4 * 60 * 60, unit = 's')#, format = "%Y-%m-%d")
     event = event.drop(['two_again', 'device_type_again', 'device_id_again', 'zero_again', '10-digit-int','url_session_num','int_2_to_1101','file_path?','NaNs','zero','weird_hash_thing','two','13-digit_int'], axis=1)
@@ -25,76 +27,6 @@ def get_data():
     readings.set_index('event_id', inplace = True)
     readings = readings.pivot(columns = 'measurment', values = 'value')
     return event.join(readings).sort_values('timestamp')
-
-def is_in_ny(data):
-    '''
-    data = DataFrame with lat long as column names
-
-    Returns Series object specifying 1 for if the point is in NYC boundry box
-    Note to self: this would be improved if it could specify stricter shapes or neighborhoods
-    '''
-    lat_boundry = (40.477399, 40.917577)
-    lon_boundry = (-74.25909, -73.700009)
-    data['in_ny'] = (data['lat']>= 40.47739) & (data['lat']<=40.917577) & (data['long'] >= -74.25909) & (data['long'] <= -73.700009)
-
-    return data
-
-
-def get_location(coords):
-    '''
-    INPUT: list of tuples containing (latitude, longitude)
-    OUTPUT: list of strings containing response from openstreetmap
-
-    This function uses a reverse geocode lookup from https://nominatim.openstreetmap.org
-    '''
-    start = time()
-    loc = []
-    ct = 0
-    for lat, lon in coords:
-        if ct % 100 == 0:
-            print "on event {}".format(ct)
-        ct += 1
-        try:
-            r = requests.get("https://nominatim.openstreetmap.org/reverse.php?format=html&lat={}&lon={}&zoom=".format(lat,lon))
-            soup = BeautifulSoup(r.content, 'html.parser')
-            loc.append(soup.find_all(class_="name")[0].text)
-        except:
-            loc.append("not available")
-    print "completed in {} seconds".format(time() - start)
-    return loc
-
-def get_city_state_country(coords):
-    '''
-    mostly useless because google api is stingy
-    '''
-    gmaps_key = os.environ.get("gmaps_key")
-    json_decoder = json.JSONDecoder()
-    cities = []
-    states = []
-    countries = []
-    counter = 0
-    for lat, lon in coords:
-        if counter % 100 == 0:
-            print "on user {}".format(counter)
-        counter += 1
-        r = requests.get("https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key={}".format(lat,lon, gmaps_key))
-        content = json_decoder.decode(r.content)
-        try:
-            cities.append(str(content['results'][0]['address_components'][3]['long_name'].encode('ascii',errors = 'ignore')))
-        except:
-            cities.append('not available')
-        try:
-            states.append(str(content['results'][0]['address_components'][4]['long_name'].encode('ascii',errors = 'ignore')))
-        except:
-            states.append('not available')
-        try:
-            countries.append(str(content['results'][0]['address_components'][5]['long_name']))
-        except:
-            countries.append('not available')
-    pickle.dump(cities, open('cities.pkl', 'wb'))
-    pickle.dump(states, open('states.pkl', 'wb'))
-    pickle.dump(countries, open('countries.pkl', 'wb'))
-    return cities, states, countries
 
 def get_single_device(device_id):
     '''
@@ -129,11 +61,11 @@ def plot_single_device(device_id):
     data = get_single_device(device_id)
     rides = data.ride.unique()
     lat = data.lat.mean()
-    lon = data.long.mean()
+    lon = data.lon.mean()
     m = folium.Map((lat,lon), zoom_start = 12)
     for ride in rides:
         temp = data[data.ride == ride]
-        coords = zip(temp.lat,temp.long)
+        coords = zip(temp.lat,temp.lon)
         time = temp.timestamp
         plot_single_ride(m, coords, time)
     m.save("plots/devices/{}".format(device_id))
@@ -156,7 +88,71 @@ def plot_single_ride(m, coords, time = None, interval = 5):
     folium.Marker(coords[-1], popup = "End").add_to(m)
     return m
 
-# def
+def find_major_cities(df):
+    db = DBSCAN(eps = .1, min_samples = 100) # eps 1 = 111.2KM, 0.1 = 11.12KM
+    labels = db.fit_predict(df[['lat','lon']])
+    labels = np.expand_dims(labels, axis = 1)
+    df['county'] = labels
+    geocoder = geopy.geocoders.Nominatim()
+    counties = {}
+    centroids = {}
+    exceptions = []
+    for label in np.unique(labels):
+        X = df[df.county == label][['lat','lon']]
+        lat, lon = X.mean()
+        location = geocoder.reverse((lat,lon))
+        try:    # Works for most countries
+            counties[label] = location.raw['address']['county']
+            centroids[label] = (lat,lon)
+        except:
+            try:    # UK and CA use suburb instead of county
+                counties[label] = location.raw['address']['suburb']
+                centroids[label] = (lat,lon)
+            except:     # Sweeden and Romania are in unicode so this is done by hand
+                if (lat,lon) == (37.465285558094635, 126.95470294348861):
+                    counties[label] = 'Sweeden'
+                    centroids[label] = (lat,lon)
+                elif (lat,lon) == (44.435190075805657, 26.093614125129637):
+                    counties['label'] = 'Romania'
+                    centroids[label] = (lat,lon)
+                else:   # One set has weid behavior 923 events over 52 devices
+                    exceptions.append((lat,lon))
+                    counties[label] = (lat,lon)
+                    centroids[label] = (lat,lon)
+    df['city_center'] = df.county.map(centroids)
+    df.county = df.county.map(counties)
+    return exceptions
+
+
+
+def find_city_centroids(df, n_clusters = 8, pollutant = 'PM25'):
+    '''
+    INPUT:  df = a dataframe with lat, lon, timestamp and [pollutant] columns
+            n_clusters = the number of clusters to find
+            pollutant = the type of pollutant to filter by
+    OUTPUT: None
+
+    This function appends a column to df with the lat lon of the nearest centroid
+    '''
+    mask = np.ma.masked_invalid(df[pollutant])
+    X = df[~mask.mask][['lat', 'lon', 'timestamp']]
+    start = X.timestamp.min()
+    X['seconds'] = X.timestamp - start
+    X['seconds'] = X.seconds.apply(lambda x: x.total_seconds())
+    ts = X.pop('timestamp')
+    X = (X - X.mean())/X.std()
+
+    estimator = KMeans(n_clusters = n_clusters)
+    labels = estimator.fit_predict(X)
+    # labels = np.expand_dims(labels, axis = 1)
+    X['centroid'] = labels
+    df = df.join(X.centroid)
+    centroids = {}
+    for label in np.unique(labels):
+        temp = df[df.centroid == label][['lat', 'lon']]
+        lat, lon = temp.mean()
+        centroids[label] = (lat, lon)
+    df['centroid'] = df['centroid'].map(centroids)
 
 # terminal command to connect to db and download event table and convert to csv
 # psql -h 104.196.143.165 -U multitude -d d2c_api_prod -p 5432 -o event.csv -c 'select * from event;' -P format=unaligned -P tuples_only -P fieldsep=\,
@@ -165,25 +161,27 @@ def plot_single_ride(m, coords, time = None, interval = 5):
 # psql -h <host_ip> -U <username> -d <database> -p <port> -o <output_file_path> -c '<query>' -P format=unaligned -P tuples_only -P fieldsep=\,
 
 if __name__ == '__main__':
-    event = get_data()
-    # coords = zip(event.lat,event.long)
-    # locations = get_location(coords)
-    # event['location'] = locations
-    pickle.dump(event, open('event.pkl', 'wb'))
-    d1 = get_single_device('SAMSUNG-SM-G920A:1083')
-    ny = event[event['in_ny'] == True]
+    # event = get_data()
+    # exceptions = find_major_cities(event)
+    # event.to_pickle('event.pkl')
+    event = pickle.load(open('event.pkl','rb'))
+
+    # d1 = get_single_device('SAMSUNG-SM-G920A:1083')
+    ny = event[event['county'] == "New York County"]
+    find_city_centroids(ny)
     # lat = d1.lat.mean()
-    # lon = d1.long.mean()
+    # lon = d1.lon.mean()
     # days = d1.timestamp.dt.date.unique()
     # for n, day in enumerate(days):
     #     m = folium.Map((lat,lon), zoom_start = 13)
     #     temp = d1[d1.timestamp.dt.date == day]
-    #     coords = zip(temp.lat,temp.long)
+    #     coords = zip(temp.lat,temp.lon)
     #     folium.PolyLine(coords).add_to(m)
     #     folium.Marker(coords[0], popup = 'Start').add_to(m)
     #     folium.Marker(coords[-1], popup = "End").add_to(m)
     # m.save('plots/devices/{}.html'.format(device_id))
-    """Same user for 'Nexus-6P:1008' and 'AirBeam:001896105F77'"""
+    """Same user for 'Nexus-6P:1008' and 'AirBeam:001896105F77'
+        ?Same user for 'Terrier-0006667BB110' and 'SAMSUNG-SM-G920A/1083'"""
 
     # plot_single_device('Nexus-6P:1008')
     # plot_single_device('AirBeam:001896105F77')
